7c12e1e8902bb2937e259a3bd4777b6b5439f001
Panda <64888060+RouxPanda@users.noreply.github.com>
2021-01-06 11:51:46 +0100

    début des recherches d'annonces

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -40,21 +40,51 @@ class Home extends BaseController
 			throw new \CodeIgniter\Exceptions\PageNotFoundException($page);
 		}
 
+		if ($this->request->getMethod() == 'post') {
+			$recup = $this->request->getVar('rchr');
+			return redirect()->to(base_url()."/Home/search/$recup");
+		}
+
 		$this->smarty = service('SmartyEngine');
 		$this->smarty->assign("title", ucfirst($page));
 		
 		$annonceModel = new \App\Models\AnnonceModel();
 		$datas = array();
 
-		if($page == 'annonces') {
-			$datas = $annonceModel->findAll(16, $numero*16);
-		}
+		$datas = $annonceModel->findAll(16, $numero*16);
+		
 
 		$this->smarty->assign("datas", $datas);
 		$this->smarty->assign("numero", $numero);
 		$this->smarty->assign("total", count($datas));
 
 		return $this->smarty->view('pages/home/'.$page.'.tpl'); 
-    }
+	}
+	
+	public function search($recherche) {
+        $page = 'annonces';
+        $session = session();
+
+		if ( ! is_file(APPPATH.'/Views/pages/home/'.$page.'.tpl'))
+		{
+			throw new \CodeIgniter\Exceptions\PageNotFoundException($page);
+		}
+
+		$this->smarty = service('SmartyEngine');
+		$this->smarty->assign("title", ucfirst($page));
+		
+		$annonceModel = new \App\Models\AnnonceModel();
+		$datas = array();
+		$datas = $annonceModel->where("A_titre LIKE '%$recherche%'")->findAll();
+		if(empty($datas)){
+			$session->setFlashdata("error", array('Aucunes annonce trouvé'));
+		}
+		
+
+		$this->smarty->assign("datas", $datas);
+		$this->smarty->assign("total", count($datas));
+
+		return $this->smarty->view('pages/home/'.$page.'.tpl'); 
+	}
 
 }
--- app/Views/pages/home/annonces.tpl
+++ app/Views/pages/home/annonces.tpl
@@ -1,6 +1,17 @@
 {include file='templates/header.tpl'}
 
     <section>
+        <div class="row centre_bas_page">
+            <form class="custom-form" action="{base_url()}/Home/annonces" method="post" style="width:40%;">
+                <div class="input-group md-form form-sm form-1 pl-0">
+                  <div class="input-group-prepend">
+                    <button class="input-group-text cyan lighten-2" type="submit" id="basic-text1">
+                        <i class="fas fa-search text-white" aria-hidden="true"></i></button>
+                  </div>
+                  <input class="form-control my-0 py-1" type="text" name="rchr" placeholder="Recherche" aria-label="Recherche">
+                </div>
+            </form>
+        </div>
         <div class="row" style="text-align: left">
             {foreach from=$datas item=$annonce}
             
